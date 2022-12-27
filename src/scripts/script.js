9b037b73866c3d960bc495d853b740392da35d4c
Luis Gabriel da Silva Araújo <luis.gabriel@fitbank.com.br>
2022-12-27 13:33:34 -0300

    updating

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -2,5 +2,7 @@
 
 function a() {
     window.prompt("Quanto de vida você deseja que rael tenha?");
+    window.prompt("Quanto de vida você tem?");
+    window.prompt("Quantas vidas você tem?");
     window.location.href = "https://www.youtube.com/watch?v=a3Z7zEc7AXQ"
 }
